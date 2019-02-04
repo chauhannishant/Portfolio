63295ce6fd4f3775c3e15376987858ef3b595d36
Nishant Chauhan <nishantyam1@gmail.com>
2019-02-04 23:54:48 +0530

    project structure added

--- src/components/project.js
+++ src/components/project.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {withPrefix} from 'gatsby'
 
+
 const Project = (props) => (
     <div className="mainProj">
+        <img className="projImg" alt={props.alt} src={withPrefix(props.imageName)}/>
         <div className="projText">
-            {/* {props.children} */}
+            {props.children}
         </div>
-        <img alt={props.alt} src={withPrefix(props.imageName)}/>
     </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,27 @@ const IndexPage = () => (
       <h1>Hello, I am Nishant</h1>
       <p>I am a digital product designer with a focus on User Experience. I am passionate about learning new things & belive a designer in isolation starts to rot. I have learnt a lot from my teams in the past and gained the skills to build this portfolio with help from my team at Cleartrip.</p>
     </div>
-    <Project imageName="/projectOne.png" alt="Project Image"/>
-    <Project imageName="/nishant.png" alt="Project Image">
-      <h3>sdjrghfdh</h3>
-      <p>sd,gmdfjhakjdhgvc</p>
-  </Project>
-  </Layout>
+    <Project className="oddProj" imageName="/srp-thumb.png" alt="SRP Thumbnail">
+      <h3>Project Title 1</h3>
+      <p>Brief explanation on what we set out to do and what was the main challenge on the outset. Brief explanation on what we set out to do and what was the main challenge on the outset.</p>
+    </Project>
+    <Project imageName="/srp-thumb.png" alt="SRP Thumbnail">
+      <h3>Project Title 2</h3>
+      <p>Brief explanation on what we set out to do and what was the main challenge on the outset. Brief explanation on what we set out to do and what was the main challenge on the outset.</p>
+    </Project>
+    <Project className="oddProj" imageName="/srp-thumb.png" alt="SRP Thumbnail">
+      <h3>Project Title 3</h3>
+      <p>Brief explanation on what we set out to do and what was the main challenge on the outset. Brief explanation on what we set out to do and what was the main challenge on the outset.</p>
+    </Project>
+    <Project imageName="/srp-thumb.png" alt="SRP Thumbnail">
+      <h3>Project Title 4</h3>
+      <p>Brief explanation on what we set out to do and what was the main challenge on the outset. Brief explanation on what we set out to do and what was the main challenge on the outset.</p>
+    </Project>
+    <Project className="oddProj" imageName="/srp-thumb.png" alt="SRP Thumbnail">
+      <h3>Project Title 5</h3>
+      <p>Brief explanation on what we set out to do and what was the main challenge on the outset. Brief explanation on what we set out to do and what was the main challenge on the outset.</p>
+    </Project>
+</Layout>
 )
 
 export default IndexPage
